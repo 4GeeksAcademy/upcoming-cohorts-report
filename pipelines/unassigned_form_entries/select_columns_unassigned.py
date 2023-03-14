35fee4e14d43de181d921edb3c82710a230464c6
vmollerkroj <vmoller@4geeksacademy.com>
2023-03-14 23:00:14 +0000

    Changed column name

--- pipelines/unassigned_form_entries/select_columns_unassigned.py
+++ pipelines/unassigned_form_entries/select_columns_unassigned.py
@@ -8,7 +8,7 @@ course = ['course_1', 'course_2']
 language = ['en', 'es']
 country = ['Country_1', 'Country_2']
 city = ['City_1', 'City_2']
-created_at = ['2020-09-22 05:27:37.584999', '2022-11-23 12:24:36.586999']
+form_created_at = ['2020-09-22 05:27:37.584999', '2022-11-23 12:24:36.586999']
 lead_type = ['STRONG', 'SOFT']
 location = ['Location_1', 'Location_2']
 ac_deal_owner_full_name = ['Owner_1', 'Owner_2']
@@ -17,7 +17,7 @@ cohort_assignation_error = ['Missing academy', np.nan]
 academy = [np.nan, 'Academy_2']
 
 dict_inputs = {'email':email, 'course':course, 'language':language, 'country':country, 'city':city,
-                'created_at':created_at, 'lead_type':lead_type, 'location':location, 'ac_deal_owner_full_name':ac_deal_owner_full_name,
+                'form_created_at':form_created_at, 'lead_type':lead_type, 'location':location, 'ac_deal_owner_full_name':ac_deal_owner_full_name,
                 'fullname':fullname, 'cohort_assignation_error':cohort_assignation_error, 'academy':academy}
 
 expected_inputs = pd.DataFrame(dict_inputs)
@@ -27,7 +27,7 @@ expected_inputs = pd.DataFrame(dict_inputs)
 
 fullname = ['Fullname_1', 'Fullname_2']
 email_u = ['email_1@mail.com', 'email_2@mail.com']
-created_at = ['2020-09-22 05:27:37.584999', '2022-11-23 12:24:36.586999']
+form_created_at = ['2020-09-22 05:27:37.584999', '2022-11-23 12:24:36.586999']
 course = ['course_1', 'course_2']
 language = ['en', 'es']
 academy = [np.nan, 'Academy_2']
@@ -40,7 +40,7 @@ ac_deal_owner_full_name = ['Owner_1', 'Owner_2']
 datetime_creation = [pd.to_datetime('2020-09-22 05:27:37.584999'), pd.to_datetime('2022-11-23 12:24:36.586999')]
 days_since_creation = [(pd.Timestamp.now()-pd.to_datetime('2020-09-22 05:27:37.584999')).days, (pd.Timestamp.now()-pd.to_datetime('2022-11-23 12:24:36.586999')).days]
 
-dict_output = {'fullname':fullname, 'email_u':email_u, 'created_at':created_at, 'course':course,
+dict_output = {'fullname':fullname, 'email_u':email_u, 'form_created_at':form_created_at, 'course':course,
                 'language':language, 'academy':academy, 'location':location, 'country':country, 'city':city, 'cohort_assignation_error':cohort_assignation_error, 'lead_type':lead_type,
                 'ac_deal_owner_full_name':ac_deal_owner_full_name, 'datetime_creation':datetime_creation, 'days_since_creation':days_since_creation}
 
@@ -50,11 +50,11 @@ expected_output = pd.DataFrame(dict_output)
 def run(unassigned):
 
     # Select only needed columns
-    unassigned = unassigned[['fullname', 'email', 'created_at', 'course', 'language', 'academy',  
+    unassigned = unassigned[['fullname', 'email', 'form_created_at', 'course', 'language', 'academy',  
                             'location', 'country', 'city', 'cohort_assignation_error', 'lead_type', 'ac_deal_owner_full_name']]
 
     # Create column for creation time in datetime
-    unassigned['datetime_creation'] = pd.to_datetime(unassigned['created_at'])
+    unassigned['datetime_creation'] = pd.to_datetime(unassigned['form_created_at'])
 
     # Create column that shows how many days have passed since creation
     unassigned['days_since_creation'] = (pd.Timestamp.now() - unassigned['datetime_creation']).dt.days
